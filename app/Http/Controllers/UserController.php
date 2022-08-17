85f80ae1204699e3705d27e323d3a133108e1e9a
Md. Bodiuzzaman Ripon <riponzamanbd@gmail.com>
2022-08-17 22:52:35 +0600

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    function login(Request $request){
+        $user = User::where('email',$request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(
+                [
+                    'message'=> 'Login information is invalid'
+                ],401
+            );
+        }
+        $token = $user->createToken('my-app-token')->plainTextToken;
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+        return response($response, 201);
+    }
+}
